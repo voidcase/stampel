469392bffd5fcbd8110d307f85813dc0d8b9e377
Isak Lindhé <isak.e.lindhe@gmail.com>
2019-11-25 15:40:00 +0100

    readline and more graceful exit

--- stam.py
+++ stam.py
@@ -2,13 +2,17 @@
 
 from sys import argv
 from os import system
+import readline
 
 if __name__ == '__main__':
     pattern = argv[1]
-    while True:
-        raw = input()
-        if not raw:
-            break
-        params = [p.strip() for p in raw.split(',')]
-        cmd = pattern.format(*params)
-        system(cmd)
+    try:
+        while True:
+            raw = input()
+            if not raw:
+                break
+            params = [p.strip() for p in raw.split(',')]
+            cmd = pattern.format(*params)
+            system(cmd)
+    except (EOFError, KeyboardInterrupt):
+        pass
