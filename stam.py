97c9a71270a55c17720e00859e88b4ac054030f9
Isak Lindhé <isak.e.lindhe@gmail.com>
2019-11-29 13:48:57 +0100

    prompt arrow

--- stam.py
+++ stam.py
@@ -4,11 +4,13 @@ from sys import argv
 from os import system
 import readline
 
+def show_help():
+
 if __name__ == '__main__':
     pattern = argv[1]
     try:
         while True:
-            raw = input()
+            raw = input('> ')
             if not raw:
                 break
             params = [p.strip() for p in raw.split(',')]
