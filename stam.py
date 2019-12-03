ea63160418f51d29634c616dbaa5dbc4937b0db3
Isak Lindhé <isak.e.lindhe@gmail.com>
2019-12-03 13:23:44 +0100

    addded helptext

--- stam.py
+++ stam.py
@@ -1,12 +1,23 @@
 #!/usr/bin/env python3
 
-from sys import argv
+from sys import argv, exit
 from os import system
 import readline
 
 def show_help():
+    print('Do like this: \n \
+        $ stam.py "echo and in that {} there was a {}"\n \
+        > bog, hole\n \
+        and in that bog there was a hole\n \
+        > hole, tree\n \
+        and in that hole there was a tree\n' \
+        )
 
 if __name__ == '__main__':
+    if len(argv) != 2:
+        show_help()
+        exit(1)
+        
     pattern = argv[1]
     try:
         while True:
