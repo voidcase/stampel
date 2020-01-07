355de918f3a8f75e22e9566d80facf3989786da6
Isak Lindhé <isak.e.lindhe@gmail.com>
2020-01-07 23:59:47 +0100

    bumped version, added download_url

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 from pathlib import Path
 
-VERSION = '0.0.1'
+VERSION = '0.0.2'
 HERE = Path(__file__).parent
 README = (HERE / 'README.md').read_text()
 
@@ -13,6 +13,7 @@ setup(
     long_description_content_type='text/markdown',
     license='MIT',
     url='https://github.com/voidcase/stampy',
+    download_url='https://github.com/voidcase/stampy/archive/v{}.tar.gz'.format(VERSION)
     py_modules='stampy',
     keywords=['data-entry', 'utility', 'commandline']
     entry_points={
