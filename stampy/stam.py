4fef50da0cd5c9606682d60f4b93e97cfac1daa8
Isak Lindhé <isak.e.lindhe@gmail.com>
2020-01-07 23:12:54 +0100

    proper argparsing

--- stampy/stam.py
+++ stampy/stam.py
@@ -1,34 +1,49 @@
 #!/usr/bin/env python3
 
-from sys import argv, exit
-from os import system
 import readline
+from sys import stderr
+from os import system
+from argparse import ArgumentParser, RawDescriptionHelpFormatter
 
-def show_help():
-    print('Do like this: \n \
+
+EXAMPLE = 'Do like this: \n \
         $ stam.py "echo and in that {} there was a {}"\n \
         > bog, hole\n \
         and in that bog there was a hole\n \
         > hole, tree\n \
         and in that hole there was a tree\n' \
-        )
+
+
+def get_args():
+    parser = ArgumentParser(
+        formatter_class=RawDescriptionHelpFormatter,
+        description='Execute multiple similar commands with minimal typing.',
+        epilog=EXAMPLE,
+    )
+    parser.add_argument('pattern',
+                        help='a shell command with {} at the insert positions'
+                        )
+    parser.add_argument('--delimeter', '-d', default=',')
+    return parser.parse_args()
+
 
 def run():
-    if len(argv) != 2:
-        show_help()
-        exit(1)
-        
-    pattern = argv[1]
+    args = get_args()
     try:
         while True:
             raw = input('> ')
             if not raw:
                 break
-            params = [p.strip() for p in raw.split(',')]
-            cmd = pattern.format(*params)
+            params = [p.strip() for p in raw.split(args.delimeter)]
+            try:
+                cmd = args.pattern.format(*params)
+            except IndexError:
+                print("Not enough arguments to fill all {}'s.", file=stderr)
+                continue
             system(cmd)
     except (EOFError, KeyboardInterrupt):
         pass
+
 
 if __name__ == '__main__':
     run()
