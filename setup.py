45f68d1be777be9fddd4e9e62f6bd4b0104b427c
Isak Lindhé <isak.e.lindhe@gmail.com>
2020-01-09 22:27:29 +0100

    bumped version to v0.1.0

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 from pathlib import Path
 
-VERSION = '0.0.3'
+VERSION = '0.1.0'
 HERE = Path(__file__).parent
 README = (HERE / 'README.md').read_text()
 
